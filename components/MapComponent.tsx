898076c6914e173f3491e62e5a9fe8e23f22e8bd
stormymayday <stormymayday7@gmail.com>
2025-02-01 15:12:19 -0800

    dynamic mapStyle

--- components/MapComponent.tsx
+++ components/MapComponent.tsx
@@ -2,18 +2,48 @@
 
 import { Map } from "@vis.gl/react-maplibre";
 import "maplibre-gl/dist/maplibre-gl.css";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
 export default function MapComponent() {
+    const mapStyles = ["positron", "bright", "liberty", "dark"];
+
+    const [mapStyle, setMapStyle] = useState(
+        `https://tiles.openfreemap.org/styles/${mapStyles[0]}`
+    );
+
     return (
-        <div className="w-full h-full rounded overflow-hidden">
-            <Map
-                initialViewState={{
-                    longitude: -100,
-                    latitude: 40,
-                    zoom: 3,
-                }}
-                mapStyle="https://tiles.openfreemap.org/styles/liberty"
-            />
-        </div>
+        <>
+            <div className="w-full h-full rounded overflow-hidden">
+                <Map
+                    initialViewState={{
+                        longitude: -100,
+                        latitude: 40,
+                        zoom: 3,
+                    }}
+                    mapStyle={mapStyle}
+                />
+            </div>
+            <div className="flex space-x-2 mt-4">
+                {mapStyles.map((style) => {
+                    const isActive =
+                        `https://tiles.openfreemap.org/styles/${style}` ===
+                        mapStyle;
+                    return (
+                        <Button
+                            variant={isActive ? "default" : "secondary"}
+                            key={style}
+                            onClick={() => {
+                                setMapStyle(
+                                    `https://tiles.openfreemap.org/styles/${style}`
+                                );
+                            }}
+                        >
+                            {style.charAt(0).toUpperCase() + style.slice(1)}
+                        </Button>
+                    );
+                })}
+            </div>
+        </>
     );
 }
