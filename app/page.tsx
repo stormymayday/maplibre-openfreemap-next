35df6f53f42a81943d61d41924ae0957dc3e36d1
stormymayday <stormymayday7@gmail.com>
2025-01-31 22:45:39 -0800

    shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import Map from "../components/Map";
+import Map from "@/components/Map";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
     return (
         <main className="h-[100vh] flex flex-col items-center justify-center">
-            <Map />;
+            <Map />
+            <div>
+                <Button>Click me</Button>
+            </div>
         </main>
     );
 }
